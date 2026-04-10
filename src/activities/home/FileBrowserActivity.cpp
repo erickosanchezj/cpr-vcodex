778d65365879e58b3a1cfd44cd8e4186fac720f3
franssjz <franssjz7@gmail.com>
2026-04-10 17:11:25 +0200

    refactor: Deduplicated BMP header writing in Xtc (#1439)
    **What is the goal of this PR?**
    
    Replaced manual 1-bit BMP header logic in `Xtc::generateCoverBmp()` and
    `Xtc::generateThumbBmp()` with calls to the existing `createBmpHeader()`
    utility. Added a `BmpRowOrder` enum and param to support the top-down
    row order of XTC cover images.
    
    ---
    
    While CrossPoint doesn't have restrictions on AI tools in contributing,
    please be transparent about their usage as it
    helps set the right context for reviewers.
    
    Did you use AI tools to help write this code? _**PARTIALLY**_

--- src/activities/home/FileBrowserActivity.cpp
+++ src/activities/home/FileBrowserActivity.cpp
@@ -245,6 +245,14 @@ std::string getFileName(std::string filename) {
   return filename.substr(0, pos);
 }
 
+std::string getFileExtension(std::string filename) {
+  if (filename.back() == '/') {
+    return "";
+  }
+  const auto pos = filename.rfind('.');
+  return filename.substr(pos);
+}
+
 void FileBrowserActivity::render(RenderLock&&) {
   renderer.clearScreen();
 
@@ -263,7 +271,8 @@ void FileBrowserActivity::render(RenderLock&&) {
     GUI.drawList(
         renderer, Rect{0, contentTop, pageWidth, contentHeight}, files.size(), selectorIndex,
         [this](int index) { return getFileName(files[index]); }, nullptr,
-        [this](int index) { return UITheme::getFileIcon(files[index]); });
+        [this](int index) { return UITheme::getFileIcon(files[index]); },
+        [this](int index) { return getFileExtension(files[index]); }, false);
   }
 
   // Help text
